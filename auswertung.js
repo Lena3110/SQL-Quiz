2b62bbd3f6aa42517b6405dc6361af0ae7773c26
Lena3110 <149622065+Lena3110@users.noreply.github.com>
2023-11-12 10:24:55 +0100

    Add files via upload

--- auswertung.js
+++ auswertung.js
@@ -1,3 +1,3 @@
 function showScore(elementID) {
-    document.getElementById(elementID).innerHTML = localStorage.getItem('score')
+    document.getElementById(elementID).innerHTML = localStorage.getItem('score')        // die InnerHTML wird durch den Punktescore ersetzt, welcher aus dem LocalStorage abgerufen wird
 }
